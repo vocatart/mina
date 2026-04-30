7c509a802557b2f25c0a9677d88d227e8408e0a1
vocatart <cooperoneida@u.boisestate.edu>
2026-04-30 15:41:23 -0600

    WHATEVER

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     parser.add_argument("data_dir", type=str)
 
-    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--num_workers", type=int, default=4)
 
     parser.add_argument("--conv_dim", type=int, default=256) # d_l
