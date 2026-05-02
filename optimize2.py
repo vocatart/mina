aa44d2c128318fb38a85f3f23059609855bae42e
vocatart <cooperoneida@u.boisestate.edu>
2026-05-02 15:40:18 -0600

    OH OKAY

--- optimize2.py
+++ optimize2.py
@@ -158,6 +158,7 @@ def trainable(config: dict):
 
 
 if __name__ == "__main__":
+    torch.serialization.add_safe_globals([PositionalEncodingType])
     parser = argparse.ArgumentParser()
     parser.add_argument("data_dir")
     parser.add_argument("--batch_size", type=int, default=16)
